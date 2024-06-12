4529df6e0dfbe135017c20852569a9a770212da4
AlineRG <aline.rabago@gmail.com>
2024-06-11 21:52:06 -0400

    importar modulos para crear aplicacion web con Flask

--- myapp/app.py
+++ myapp/app.py
@@ -1,2 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "mysecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myDB.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 
