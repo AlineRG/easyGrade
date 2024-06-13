09d2a3b30ed61ea00b32518434cab1248b4330c3
AlineRG <aline.rabago@gmail.com>
2024-06-13 12:47:02 -0400

    Definir modelo de usuario con id, username, email y password

--- myapp/app.py
+++ myapp/app.py
@@ -22,3 +22,11 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+
+
+#Definir modelo de usuario
+class User(db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    username = db.Column(db.String(30), unique = True, nullable = False)
+    email = db.Column(db.String(30), unique = True, nullable = False)
+    password = db.Column(db.String(50), nullable=False)
