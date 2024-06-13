bd53b121ef2c7bc0e0653ca76f97d2e9b71edf89
AlineRG <aline.rabago@gmail.com>
2024-06-13 13:39:52 -0400

    Agregar funcionalidad de login y ruta de registro

--- myapp/app.py
+++ myapp/app.py
@@ -22,6 +22,17 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            flash('Inicio de sesión exitoso!')
+            return redirect(url_for('index'))
+        else:
+            flash('Usuario o Contraseña incorrectos')
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
 
 
 #Definir modelo de usuario
@@ -30,3 +41,16 @@ class User(db.Model):
     username = db.Column(db.String(30), unique = True, nullable = False)
     email = db.Column(db.String(30), unique = True, nullable = False)
     password = db.Column(db.String(50), nullable=False)
+
+#Formulario de registro
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Sign Up')
+
+#Formulario de inicio de sesion
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Log In')
