73441bd518b7e21ab66e3202fe21d7bdcd56d71d
AlineRG <aline.rabago@gmail.com>
2024-06-20 12:04:23 -0400

    Cambie modelo de usuario primero y las rutas al final

--- myapp/app.py
+++ myapp/app.py
@@ -13,6 +13,27 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
+#Modelo de usuario
+class User(db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    username = db.Column(db.String(30), unique = True, nullable = False)
+    email = db.Column(db.String(30), unique = True, nullable = False)
+    password = db.Column(db.String(50), nullable=False)
+
+#Formulario de registro
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Sign Up')
+
+#Formulario de inicio de sesion
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Log In')
+
 #Routes 
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -24,7 +45,7 @@ def login():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and check_password_hash(user.password, login_form.password.data):
             flash('Inicio de sesion existoso')
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
         else:
             flash('Usuario o Contraseña incorrectos')
 
@@ -46,25 +67,6 @@ def register():
     return render_template('login.html', login_form=login_form, register_form=register_form)
 
 
-#Modelo de usuario
-class User(db.Model):
-    id = db.Column (db.Integer, primary_key = True)
-    username = db.Column(db.String(30), unique = True, nullable = False)
-    email = db.Column(db.String(30), unique = True, nullable = False)
-    password = db.Column(db.String(50), nullable=False)
-
-#Formulario de registro
-class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
-    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
-    submit = SubmitField('Sign Up')
-
-#Formulario de inicio de sesion
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=10, max=50)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
-    submit = SubmitField('Log In')
 
 @app.route('/')
 def index():
