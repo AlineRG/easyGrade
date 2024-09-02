8456c1419a8f1d4aa21a641db960c556b36be110
AlineRG <aline.rabago@gmail.com>
2024-09-01 21:58:50 -0400

    def get_alumnos_data_by_materia_id

--- database/query.py
+++ database/query.py
@@ -24,3 +24,23 @@ def get_maestros_data_by_apellido(apellido: str) -> pd.DataFrame:
     df = pd.DataFrame(result_data)
     return df
 
+
+def get_alumnos_data_by_materia_id(materia_id: int) -> pd.DataFrame:
+    """
+    This function queries the database and retrieves all the data from the table
+    ALUMNOS where MATERIA_ID == materia_id.
+
+    Args:
+    * materia_id : int. The MATERIA_ID to query
+
+    Returns:
+    * df: pd.DataFrame. A table with all the data from the ALUMNOS table
+    where MATERIA_ID is equal to the variable materia_id
+    """
+    query = f"SELECT * FROM ALUMNOS WHERE MATERIA_ID = {materia_id}"
+    result = conn.execute(query)
+
+    result_data = result.fetchall()
+    df = pd.DataFrame(result_data)
+    return df
+
