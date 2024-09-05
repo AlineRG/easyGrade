67b15bb6a549520c5dbea2744dd02fe3a574aac5
AlineRG <aline.rabago@gmail.com>
2024-09-05 13:42:57 -0400

    Change value to values

--- tests/test_query.py
+++ tests/test_query.py
@@ -4,7 +4,7 @@ from database.query import get_maestros_data_by_apellido
 
 def test_query():
     df = get_maestros_data_by_apellido("Rodriguez")
-    nombre = df["NOMBRE"].value[0]
+    nombre = df["NOMBRE"].values[0]
 
     assert isinstance(nombre, str)
     assert nombre == "Marta"
