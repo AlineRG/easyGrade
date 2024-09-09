171606f2389836c01305b60bebcdf2b374cc90fb
AlineRG <aline.rabago@gmail.com>
2024-09-09 10:52:20 -0400

    Add "test" at the beginning of the functions

--- tests/test_query.py
+++ tests/test_query.py
@@ -10,7 +10,7 @@ def test_get_maestros_data_by_apellido():
     assert nombre == "Marta"
 
 
-def get_alumnos_data_by_materia_id():
+def test_get_alumnos_data_by_materia_id():
     df = get_alumnos_data_by_materia_id(1)
     alumno = df["ALUMNO"].values[0]
 
@@ -18,7 +18,7 @@ def get_alumnos_data_by_materia_id():
     assert alumno == "Juan"
 
 
-def get_maestros_by_materia_id():
+def test_get_maestros_by_materia_id():
     df = get_maestros_by_materia_id(1)
     maestro = df["MAESTRO"].values[0]
 
@@ -26,15 +26,15 @@ def get_maestros_by_materia_id():
     assert maestro == "Marta"
 
 
-def get_average_calificacion_by_alumno_id():
     df = get_average_calificacion_by_alumno_id(1)
     calificacion = df["CALIFICACION"].values[0]
 
     assert isinstance(calificacion, int)
     assert calificacion == 8
+def test_get_average_calificacion_by_alumno_id():
 
 
-def get_materias_by_maestro_id():
+def test_get_materias_by_maestro_id():
     df = get_materias_by_maestro_id(1)
     materia = df["MATERIA"].values[0]
 
@@ -42,7 +42,7 @@ def get_materias_by_maestro_id():
     assert materia == "Matematicas"
 
 
-def count_alumnos_by_maestro_id():
+def test_count_alumnos_by_maestro_id():
     df = count_alumnos_by_maestro_id(1)
     alumnos = df["ALUMNOS"].values[0]
 
@@ -50,7 +50,8 @@ def count_alumnos_by_maestro_id():
     assert alumnos == "2"
 
 
-def get_tareas_by_alumno_id():
+
+def test_get_tareas_by_alumno_id():
     df = get_tareas_by_alumno_id(1)
     tareas = df["TAREAS"].values[0]
 
@@ -58,9 +59,9 @@ def get_tareas_by_alumno_id():
     assert tareas == "Tarea 1, Tarea 2"
 
 
-def get_alumnos_ordered_by_apellido_nombre():
     df = get_alumnos_ordered_by_apellido_nombre()
     alumno = df["ALUMNO"].values[0]
 
     assert isinstance(alumno, str)
     assert alumno == "Ramirez"
+def test_get_alumnos_by_materia_ordered_by_apellido_nombre(materia_id: int):
