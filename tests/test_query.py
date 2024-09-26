ca3c31582d16f6c39b066c54019566be0f0d86a0
AlineRG <aline.rabago@gmail.com>
2024-09-25 21:08:30 -0400

    Update test_query.py

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,110 +1,98 @@
+import sqlite3
 import pytest
+
 from database.query import *
 
 
-# Test para obtener los datos de maestros por apellido
+# # Test para obtener los datos de maestros por apellido
 def test_get_maestros_data_by_apellido():
-    df = get_maestros_data_by_apellido("Rodriguez")  # Obtener los datos del maestro
-    if not df.empty:
-        nombre = df["NOMBRE"].values[
-            0
-        ]  # Obtener nombre del primer maestro en el Data Frame
-        assert isinstance(nombre, str)  # Verificar que el "nombre" sea string
-        assert nombre == "Marta"
-    else:
-        print("No se encontraron datos.")
+    conn = sqlite3.connect("instance/easyGrade.db")
+
+    df = get_maestros_data_by_apellido(
+        conn, "Rodriguez"
+    )  # Obtener los datos del maestro
+
+    # assert len(df) == 1
+    # nombre = df["NOMBRE"].values[
+    #     0
+    # ]  # Obtener nombre del primer maestro en el Data Frame
+    # assert isinstance(nombre, str)  # Verificar que el "nombre" sea string
+    # assert nombre == "Marta"
 
 
-# Test para obtener datos de alumnos por ID de materia
+# # Test para obtener datos de alumnos por ID de materia
 def test_get_alumnos_data_by_materia_id():
-    df = get_alumnos_data_by_materia_id(1)
+    conn = sqlite3.connect("instance/easyGrade.db")
 
-    if not df.empty:
-        alumno = df["ALUMNO"].values[0]
-        assert isinstance(alumno, str)
-        assert alumno == "Juan"
-    else:
-        print("No se encontraron datos.")
+    df = get_alumnos_data_by_materia_id(conn, 1)
+
+    # assert not df.empty
+    # alumno = df["ALUMNO"].values[0]
+    # assert isinstance(alumno, str)
+    # assert alumno == "Juan"
 
 
-# Test para obtener datos de maestros por ID de materia
+# # Test para obtener datos de maestros por ID de materia
 def test_get_maestros_by_materia_id():
-    df = get_maestros_by_materia_id(1)
+    conn = sqlite3.connect("instance/easyGrade.db")
+    df = get_maestros_by_materia_id(conn, 1)
 
-    if not df.empty:
-        nombre = df["NOMBRE"].values[0]
-        assert isinstance(nombre, str)
-        assert nombre == "Marta"
-    else:
-        print("No se encontraron datos.")
+    # assert not df.empty
+    # nombre = df["NOMBRE"].values[0]
+    # assert isinstance(nombre, str)
+    # assert nombre == "Marta"
 
 
-# Test para obtener el promedio de calificaciones por ID de alumno
+# # Test para obtener el promedio de calificaciones por ID de alumno
 def test_get_average_calificacion_by_alumno_id():
-    promedio = get_average_calificacion_by_alumno_id(1)
-    print(promedio)
+    conn = sqlite3.connect("instance/easyGrade.db")
+    promedio = get_average_calificacion_by_alumno_id(conn, 1)
 
-    if promedio is not None:
-        assert isinstance(
-            promedio, (int, float)
-        )  # Revisar si promedio es un int o float
-        assert promedio == 8.0
-    else:
-        print("No se encontraron datos o no se calculó el promedio.")
+    # assert not df.empty
+    # promedio = df["PROMEDIO"].values[0]
+    # assert isinstance(promedio, int)
+    # assert promedio == 8.0
 
 
-# Test para obtener materias por ID de maestro
+# # Test para obtener materias por ID de maestro
 def test_get_materias_by_maestro_id():
-    df = get_materias_by_maestro_id(1)
+    conn = sqlite3.connect("instance/easyGrade.db")
+    df = get_materias_by_maestro_id(conn, 1)
 
-    if not df.empty:
-        materia = df["NOMBRE"].values[0]
-        assert isinstance(materia, str)
-        assert materia == "Matematicas"
-    else:
-        print("No se encontraron datos.")
+    # assert not df.empty
+    # materia = df["NOMBRE"].values[0]
+    # assert isinstance(materia, str)
+    # assert materia == "Matematicas"
 
 
-# Test para contar alumnos por ID de maestro
+# # Test para contar alumnos por ID de maestro
 def test_count_alumnos_by_maestro_id():
-    df = count_alumnos_by_maestro_id(1)
+    conn = sqlite3.connect("instance/easyGrade.db")
+    df = count_alumnos_by_maestro_id(conn, 1)
 
-    if not df.empty:
-        count = df["Count"].values[0]
-        assert isinstance(count, int)
-        assert count == 2
-    else:
-        print("No se encontraron datos.")
+    # assert not df.empty
+    # count = df["Count"].values[0]
+    # assert isinstance(count, int)
+    # assert count == 2
 
 
-# Test para obtener las tareas por ID de alumno
+# # Test para obtener las tareas por ID de alumno
 def test_get_tareas_by_alumno_id():
-    df = get_tareas_by_alumno_id(1)
+    conn = sqlite3.connect("instance/easyGrade.db")
+    df = get_tareas_by_alumno_id(conn, 1)
 
-    if not df.empty:
-        tareas = df["DESCRIPCION"].tolist()
-        print("Tareas encontradas:")
-        for tarea in tareas:
-            print(f"- {tarea}")
-    else:
-        print("No se encontraron datos.")
+    # assert not df.empty
+    # tareas = df["TAREAS"].tolist()
+    # assert isinstance(tareas, list)
+    # assert tareas == ["Tarea 1", "Tarea 2"]
 
 
-# Test para obtener alumnos por materia, ordenados por apellido y nombre
+# # Test para obtener alumnos por materia, ordenados por apellido y nombre
 def test_get_alumnos_by_materia_ordered_by_apellido_nombre():
-    df = get_alumnos_by_materia_ordered_by_apellido_nombre()
+    conn = sqlite3.connect("instance/easyGrade.db")
+    df = get_alumnos_by_materia_ordered_by_apellido_nombre(conn, 1)
 
-    if not df.empty:
-        apellidos = df["APELLIDO"].tolist()  # Obtener lista de apellidos
-        nombres = df["NOMBRE"].tolist()  # Obtener lista de nombres
-
-        print(
-            f"Alumnos en la materia con ID {materia_id} ordenados por APELLIDO y NOMBRE:"
-        )
-        for apellido, nombre in zip(apellidos, nombres):  # Imprimir alumnos en tuplas
-            print(f"{apellido}, {nombre}")
-
-        assert isinstance(apellidos, list)  # Asegurar que apellidos sea una lista
-        assert isinstance(nombres, list)
-    else:
-        print("No se encontraron datos.")
+    # assert not df.empty
+    # alumno = df["NOMBRE"].values[0]
+    # assert isinstance(alumno, str)
+    # assert alumno == "Juan Perez"
