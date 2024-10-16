58877679b2a07f594aae57ea842ca2ba4c43c4d5
AlineRG <aline.rabago@gmail.com>
2024-10-16 16:19:15 -0400

    Update if_exists = "append" to if_exists="fail"
    If the table already exists, an error will be generated and no action will be taken

--- database/populate_db.py
+++ database/populate_db.py
@@ -3,3 +3,7 @@ import sqlite3
 
 
 conn = sqlite3.connect("instance/easyGrade.db")
+    df_alumnos = pd.read_csv(
+        "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/ALUMNOS.csv"
+    )
+    df_alumnos.to_sql("ALUMNOS", conn, if_exists="fail", index=False)
