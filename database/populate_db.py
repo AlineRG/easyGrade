79df82cd47de3359bf1732cd81f9c06b3028008b
AlineRG <aline.rabago@gmail.com>
2024-10-16 16:34:42 -0400

    Add exception handling for CALIFICACIONES_EJERCICIOS data

--- database/populate_db.py
+++ database/populate_db.py
@@ -12,3 +12,15 @@ try:
     print("Se han cargado los datos de ALUMNOS.csv correctamente")
 except Exception as error:
     print("No se han cargado los datos de ALUMNOS.csv correctamente:", error)
+
+try:
+    df = pd.read_csv(
+        "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CALIFICACIONES_EJERCICIOS.csv"
+    )
+    df.to_sql("CALIFICACIONES_EJERCICIOS", conn, if_exists="fail", index=False)
+    print("Se han cargado los datos de CALIFICACIONES_EJERCICIOS.csv correctamente")
+except Exception as error:
+    print(
+        "No se han cargado los datos de CALIFICACIONES_EJERCICIOS.csv correctamente:",
+        error,
+    )
