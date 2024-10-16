8d989f7f2154996e1d0bb7278d18b0d147d16c1d
AlineRG <aline.rabago@gmail.com>
2024-10-16 16:21:10 -0400

    Add Exception

--- database/populate_db.py
+++ database/populate_db.py
@@ -3,7 +3,12 @@ import sqlite3
 
 
 conn = sqlite3.connect("instance/easyGrade.db")
+
+try:
     df_alumnos = pd.read_csv(
         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/ALUMNOS.csv"
     )
     df_alumnos.to_sql("ALUMNOS", conn, if_exists="fail", index=False)
+    print("Se han cargado los datos de ALUMNOS.csv correctamente")
+except Exception as error:
+    print("No se han cargado los datos de ALUMNOS.csv correctamente:", error)
