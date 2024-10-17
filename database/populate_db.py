36a94e75ecdda1f56a8228b61c4a60bc59e596b2
AlineRG <aline.rabago@gmail.com>
2024-10-16 21:26:09 -0400

    Create function in populate_db.py

--- database/populate_db.py
+++ database/populate_db.py
@@ -4,134 +4,27 @@ import sqlite3
 
 conn = sqlite3.connect("instance/easyGrade.db")
 
-# try:
-#     df_alumnos = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/ALUMNOS.csv"
-#     )
-#     df_alumnos.to_sql("ALUMNOS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de ALUMNOS.csv correctamente")
-# except Exception as error:
-#     print("No se han cargado los datos de ALUMNOS.csv correctamente:", error)
 
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CALIFICACIONES_EJERCICIOS.csv"
-#     )
-#     df.to_sql("CALIFICACIONES_EJERCICIOS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de CALIFICACIONES_EJERCICIOS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de CALIFICACIONES_EJERCICIOS.csv correctamente:",
-#         error,
-#     )
+def create_table_if_not_exists(table_name: str) -> None:
+    query = f"SELECT COUNT(*)FROM sqlite_master WHERE type= 'table' AND name = '{table_name}'"
+    result = conn.execute(query)
+    result_data = result.fetchall()[0][0]
+    print(result_data)
+    print(type(result_data))
 
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CALIFICACIONES_EXAMENES.csv"
-#     )
-#     df.to_sql("CALIFICACIONES_EXAMENES", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de CALIFICACIONES_EXAMENES.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de CALIFICACIONES_EXAMENES.csv correctamente:",
-#         error,
-#     )
+    if result_data == 0:
+        df = pd.read_csv(
+            f"C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/{table_name}"
+        )
+        df.to_sql(table_name, conn, if_exists="append", index=False)
+        print(f"Se han cargado los datos de {table_name} correctamente")
+    elif result_data > 0:
+        print(f"La tabla {table_name} ya existe en la base de datos")
 
 
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CALIFICACIONES_MATERIA.csv"
-#     )
-#     df.to_sql("CALIFICACIONES_MATERIA", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de CALIFICACIONES_MATERIA.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de CALIFICACIONES_MATERIA.csv correctamente:",
-#         error,
-#     )
-
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CALIFICACIONES_TAREAS.csv"
-#     )
-#     df.to_sql("CALIFICACIONES_TAREAS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de CALIFICACIONES_TAREAS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de CALIFICACIONES_TAREAS.csv correctamente:",
-#         error,
-#     )
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/CONTACTO.csv"
-#     )
-#     df.to_sql("CONTACTO", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de CONTACTO.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de CONTACTO.csv correctamente:",
-#         error,
-#     )
-
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/MAESTROS.csv"
-#     )
-#     df.to_sql("MAESTROS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de MAESTROS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de MAESTROS.csv correctamente:",
-#         error,
-#     )
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/MATERIAS.csv"
-#     )
-#     df.to_sql("MATERIAS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de MATERIAS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de MATERIAS.csv correctamente:",
-#         error,
-#     )
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/REGISTRO_MATERIAS_ALUMNOS.csv"
-#     )
-#     df.to_sql("REGISTRO_MATERIAS_ALUMNOS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de REGISTRO_MATERIAS_ALUMNOS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de REGISTRO_MATERIAS_ALUMNOS.csv correctamente:",
-#         error,
-#     )
-
-# try:
-#     df = pd.read_csv(
-#         "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/REGISTRO_MATERIAS_MAESTROS.csv"
-#     )
-#     df.to_sql("REGISTRO_MATERIAS_MAESTROS", conn, if_exists="append", index=False)
-#     print("Se han cargado los datos de REGISTRO_MATERIAS_MAESTROS.csv correctamente")
-# except Exception as error:
-#     print(
-#         "No se han cargado los datos de REGISTRO_MATERIAS_MAESTROS.csv correctamente:",
-#         error,
-#     )
-
-try:
-    df = pd.read_csv(
-        "C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/TAREAS.csv"
-    )
-    df.to_sql("TAREAS", conn, if_exists="fail", index=False)
-    print("Se han cargado los datos de TAREAS.csv correctamente")
-except Exception as error:
-    print(
-        "No se han cargado los datos de TAREAS.csv correctamente:",
-        error,
-    )
+create_table_if_not_exists("ALUMNOS")
+create_table_if_not_exists("MAESTROS")
+create_table_if_not_exists("MATERIAS")
+create_table_if_not_exists("CALIFICACIONES_EJERCICIOS")
+create_table_if_not_exists("REGISTRO_MATERIAS_ALUMNOS")
+create_table_if_not_exists("REGISTRO_MATERIAS_MAESTROS")
