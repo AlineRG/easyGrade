38b7f9367ed228b89a7d4fd1e16207952e48e6aa
AlineRG <aline.rabago@gmail.com>
2024-10-23 18:12:06 -0400

    Update populate_db.py

--- database/populate_db.py
+++ database/populate_db.py
@@ -1,7 +1,8 @@
+import os
 import pandas as pd
 import sqlite3
 
-
+print(os.getcwd())
 conn = sqlite3.connect("instance/easyGrade.db")
 
 
@@ -14,7 +15,7 @@ def create_table_if_not_exists(table_name: str) -> None:
 
     if result_data == 0:
         df = pd.read_csv(
-            f"C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/{table_name}"
+            f"C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/{table_name}.csv"
         )
         df.to_sql(table_name, conn, if_exists="append", index=False)
         print(f"Se han cargado los datos de {table_name} correctamente")
@@ -25,6 +26,13 @@ def create_table_if_not_exists(table_name: str) -> None:
 create_table_if_not_exists("ALUMNOS")
 create_table_if_not_exists("MAESTROS")
 create_table_if_not_exists("MATERIAS")
+create_table_if_not_exists("CONTACTO")
+create_table_if_not_exists("TAREAS")
 create_table_if_not_exists("CALIFICACIONES_EJERCICIOS")
+create_table_if_not_exists("CALIFICACIONES_EXAMENES")
+create_table_if_not_exists("CALIFICACIONES_MATERIA")
+create_table_if_not_exists("CALIFICACIONES_TAREAS")
 create_table_if_not_exists("REGISTRO_MATERIAS_ALUMNOS")
 create_table_if_not_exists("REGISTRO_MATERIAS_MAESTROS")
+create_table_if_not_exists("EXAMENES")
+create_table_if_not_exists("ALUMNOS_MATERIAS")
