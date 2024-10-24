b0597be19a5d8a2f41a8a659f182f854818dd335
AlineRG <aline.rabago@gmail.com>
2024-10-23 20:42:55 -0400

    made path relative

--- database/populate_db.py
+++ database/populate_db.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import sqlite3
 
-print(os.getcwd())
+work_dir = os.getcwd()
 conn = sqlite3.connect("instance/easyGrade.db")
 
 
@@ -14,9 +14,7 @@ def create_table_if_not_exists(table_name: str) -> None:
     print(type(result_data))
 
     if result_data == 0:
-        df = pd.read_csv(
-            f"C:/Users/prin_/OneDrive/Documentos/easyGrade/database/csv_files/{table_name}.csv"
-        )
+        df = pd.read_csv(f"{work_dir}/database/csv_files/{table_name}.csv")
         df.to_sql(table_name, conn, if_exists="append", index=False)
         print(f"Se han cargado los datos de {table_name} correctamente")
     elif result_data > 0:
@@ -35,4 +33,4 @@ create_table_if_not_exists("CALIFICACIONES_TAREAS")
 create_table_if_not_exists("REGISTRO_MATERIAS_ALUMNOS")
 create_table_if_not_exists("REGISTRO_MATERIAS_MAESTROS")
 create_table_if_not_exists("EXAMENES")
-create_table_if_not_exists("ALUMNOS_MATERIAS")
+create_table_if_not_exists("ALUMNOS_MATERIA")
