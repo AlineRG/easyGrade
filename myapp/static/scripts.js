bfc198c132d0b71409d57d4cf671d65b225fdca0
AlineRG <aline.rabago@gmail.com>
2024-11-12 20:37:58 -0500

    Add an event listener to the "link-profile" element that triggers when clicked

--- myapp/static/scripts.js
+++ myapp/static/scripts.js
@@ -2,3 +2,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contentBox = document.querySelector(".content-box");
 
+// Add an event listener to the "link-profile" element that triggers when clicked
+    document.getElementById("link-profile").addEventListener("click", function(event) {
