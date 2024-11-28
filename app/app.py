c87ca9468a25ceb60ce9137b42444d899cc0500a
AlineRG <aline.rabago@gmail.com>
2024-11-27 20:44:52 -0500

    Add modules folder to add functions

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from app.models import db
+from app.routes import home_page, landing_page, login_register
+import app.modules
 
 
 def create_app():
@@ -9,8 +11,6 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-
-    from app.routes import home_page, landing_page, login_register
 
     app.register_blueprint(landing_page.landing_page)
     app.register_blueprint(login_register.login_register, url_prefix="/auth")
