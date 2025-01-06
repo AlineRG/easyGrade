b7f2806b22be16cf6530d993be6d469d75dadcb4
AlineRG <aline.rabago@gmail.com>
2025-01-05 20:10:23 -0500

    Add new fields to editProfile

--- app/routes/edit_profile.py
+++ app/routes/edit_profile.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Email, Length
+from wtforms.fields import DateField
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import Contacto, db
 
@@ -10,12 +11,22 @@ edit_profile_page = Blueprint("edit_profile", __name__)
 
 # Formulario de registro
 class EditProfileForm(FlaskForm):
+    nombre = StringField("Nombre", validators=[InputRequired(), Length(max=50)])
+    apellido = StringField("Apellido", validators=[InputRequired(), Length(max=50)])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(max=100)])
     telefono = StringField(
         "Telefono", validators=[InputRequired(), Length(min=5, max=50)]
     )
-    direccion = StringField("Direccion", validators=[InputRequired(), Email(), Length(max=50)])
+    direccion = StringField(
+        "Direccion", validators=[InputRequired(), Email(), Length(max=50)]
+    )
+    cumpleanos = DateField("Fecha de Nacimiento", validators=[InputRequired()])
+    rol = SelectField(
+        "Rol",
+        choices=[("estudiante", "Estudiante"), ("maestro", "Maestro")],
+        validators=[InputRequired()],
+    )
     submit = SubmitField("Guardar")
-
 
 
 @edit_profile_page.route("/edit_profile", methods=["GET", "POST"])
@@ -33,8 +44,4 @@ def edit_profile():
     #     else:
     #         flash("Usuario o Contraseña incorrectos")
 
-    return render_template(
-        "editProfile.html", edit_profile_form=edit_profile_form
-    )
-
-
+    return render_template("editProfile.html", edit_profile_form=edit_profile_form)
